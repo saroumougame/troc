c27adfe5d00a31090bea9ac3e6a9fbdb2e773be6
NegreriePaul <paul.negrerie@gmail.com>
2019-01-22 18:42:07 +0100

    ajoute du service mail

---
+++ src/Service/MailService.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sridar
+ * Date: 18/01/2019
+ * Time: 11:31
+ */
+
+namespace App\Service;
+
+
+
+class MailService
+{
+
+    private $mailer;
+
+    public function __construct(\Swift_Mailer $mailer)
+    {
+
+        $this->mailer = $mailer;
+
+    }
+
+
+    public function GetMessageEchange($statue,$echange){
+
+if ($statue === false){
+
+    $msg = 'Bonjour, '.$echange->getUserVendeur()->getUsername().' a refuser votre proposition d\'echanger le objet';
+
+}else{
+
+
+    $msg = 'Bonjour, '.$echange->getUserVendeur()->getUsername().' a accepter votre proposition d\'echanger le objet';
+
+}
+
+
+
+return $msg;
+
+
+
+    }
+
+
+
+    public function notificationMail($statue,$echange){
+
+
+        $msg = $this->GetMessageEchange($statue, $echange);
+
+        $message = (new \Swift_Message('Troc notification'))
+            ->setFrom('troc@troc.com')
+            ->setTo('sridar.aroumougame@gmail.com')
+            ->addPart(
+                $msg
+            );
+
+
+
+        return $this->mailer->send($message) > 0;
+
+
+
+    }
+
+
+}
