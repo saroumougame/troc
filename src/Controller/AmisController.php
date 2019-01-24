c0aa97d80d2a833d6bcf848bd9feec43d85174fe
Charles <cnerot91@gmail.com>
2019-01-24 14:10:21 +0100

    Added amisController

---
+++ src/Controller/AmisController.php
@@ -0,0 +1,41 @@
+<?php
+
+
+
+namespace App\Controller;
+
+use App\Entity\Amis;
+use App\Entity\Objet;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Echange;
+
+
+/**
+ * @Route("/amis")
+ */
+class AmisController extends AbstractController
+{
+    /**
+     * @Route("/show", name="amis_show")
+     */
+    public function home()
+    {
+
+      $user = $this->getUser();
+
+      $amis  = $this->getDoctrine()->getRepository(Amis::class)->findBy(array('user'=> $user->getId()));
+
+        return $this->render('amis/showAmis.html.twig',
+            array('amis' => $amis)
+        );
+
+
+
+    }
+
+
+
+
+}
